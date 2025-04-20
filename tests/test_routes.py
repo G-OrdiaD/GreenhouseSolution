3b1102fe9e1b6465a93ded8b91ae5e505e45d002
kamal rawal <kamalrawal00@gmail.com>
2025-04-20 17:12:25 -0500

    test file

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,7 +16,7 @@ class SettingsTest(unittest.TestCase):
         mock_conn.is_connected.return_value = True
         mock_db.return_value = mock_conn
 
-        # Fake form data to update thresholds
+        # mock data to update thresholds
         form_data = {
             'temperature_min': '18',
             'temperature_max': '30'
@@ -25,7 +25,7 @@ class SettingsTest(unittest.TestCase):
         # Send POST request
         response = self.client.post('/settings', data=form_data)
 
-        # Check if SQL was called correctly
+        
         self.assertIn(
             unittest.mock.call("UPDATE optimal_ranges SET min_value = %s WHERE parameter = %s", ('18', 'temperature')),
             mock_cursor.execute.call_args_list
@@ -36,7 +36,7 @@ class SettingsTest(unittest.TestCase):
             mock_cursor.execute.call_args_list
         )
 
-        # Check redirect and commit
+        # Check and commit
         mock_conn.commit.assert_called_once()
         self.assertEqual(response.status_code, 302)
 
